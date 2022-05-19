b5fc9c81b97d78e152ce773b349ded6e9e99fca3
Tonie <chaosonline@gmail.com>
2022-05-19 16:24:23 +0200

    Add project files.

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+    private readonly IConfiguration _configuration;
+
+	public MainPage(IConfiguration configuration)
+	{
+		InitializeComponent();
+        _configuration = configuration;
+    }
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        CounterBtn.Text = _configuration["myAppSetting"];
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+
+        _configuration["myAppSetting"] += "->Bye";
+	}
+}
+
---
+++ MauiApp3/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MauiApp3;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonPlatformBundle("appsettings.json")
+            .AddPlatformPreferences()
+            .Build();
+
+        var builder = MauiApp.CreateBuilder();
+
+        builder.Configuration.AddConfiguration(config);
+
+        builder.Services.AddSingleton<MainPage>();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        return builder.Build();
+    }
+}
